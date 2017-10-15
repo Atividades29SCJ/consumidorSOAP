8e6efcdaa6018600e810b2d398f8b8f018b1b37a
guilhermeandrade <guilhermeandrade.ti@gmail.com>
2017-10-14 21:23:56 -0300

    Alteração do endpoint

--- src/br/com/fiap/consumidor/load/Load.java
+++ src/br/com/fiap/consumidor/load/Load.java
@@ -8,8 +8,11 @@ public class Load {
 
 	public static void main(String[] args) {
 		
-		ConsumidorService consumidorService = new ConsumidorService();
-		Endpoint.publish("http://localhost:8080/service", consumidorService);
+		String port = System.getenv("PORT");
+        String host = "http://0.0.0.0:";
+        String uri = "/service";
+        ConsumidorService service = new ConsumidorService();
+        Endpoint.publish(host + port + uri, service);
 		
 	}
 	
